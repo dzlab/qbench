2e8dc0c09918a2ce1e91ef17b0be6d8f0a5a16e9
El-Soufy <dzlabs@outlook.com>
2016-02-16 18:32:07 +0100

    configuring Secor to push Kafka data to S3

--- tools/install_secor.sh
+++ tools/install_secor.sh
@@ -5,7 +5,23 @@ sudo mkdir ${SECOR_INSTALL_DIR}
 sudo tar -zxvf ~/secor-0.16-SNAPSHOT-bin.tar.gz -C ${SECOR_INSTALL_DIR}
 sudo mv ~/start_secor.sh ${SECOR_INSTALL_DIR}/scripts/
 rm ~/secor-0.16-SNAPSHOT-bin.tar.gz
+# configure kafka topic
 sudo sed -i 's/secor.kafka.topic_filter=.*/secor.kafka.topic_filter=auction_stream/g' ${SECOR_INSTALL_DIR}/secor.common.properties
-sudo sed -i 's/secor.s3.filesystem=s3n/secor.s3.filesystem=s3a/g' ${SECOR_INSTALL_DIR}/secor.common.properties
-sudo sed -i 's/secor.s3.bucket=/secor.s3.bucket=adomik-firehose-dump/g' ${SECOR_INSTALL_DIR}/secor.dev.properties 
-sudo sed -i 's/zookeeper.quorum=localhost/zookeeper.quorum='"$ZOOKEEPER_HOST"'/g' ${SECOR_INSTALL_DIR}/secor.dev.properties
+# configure S3 connection
+sudo sed -i 's/aws.access.key=.*/aws.access.key='"${AWS_ACCESS_KEY_ID}"'/g' ${SECOR_INSTALL_DIR}/secor.common.properties
+sudo sed -i 's/aws.secret.key=.*/aws.secret.key='"${AWS_SECRET_ACCESS_KEY}"'/g' ${SECOR_INSTALL_DIR}/secor.common.properties
+#sudo sed -i 's/secor.s3.filesystem=s3n/secor.s3.filesystem=s3a/g' ${SECOR_INSTALL_DIR}/secor.common.properties
+sudo sed -i 's/secor.s3.bucket=.*/secor.s3.bucket=adomik-firehose-dump/g' ${SECOR_INSTALL_DIR}/secor.prod.properties 
+# configure zookeeper connection
+sudo sed -i 's/zookeeper.quorum=.*/zookeeper.quorum='"$ZOOKEEPER_HOST"':2181/g' ${SECOR_INSTALL_DIR}/secor.prod.properties
+# configure kafka broker connection
+sudo sed -i 's/kafka.seed.broker.host=.*/kafka.seed.broker.host=localhost/g' ${SECOR_INSTALL_DIR}/secor.prod.properties
+sudo sed -i 's/kafka.seed.broker.port=.*/kafka.seed.broker.port=9092/g' ${SECOR_INSTALL_DIR}/secor.prod.properties
+# configure secor logging 
+sudo sed -i 's/log4j.rootLogger=.*/log4j.rootLogger=DEBUG, CONSOLE/g' ${SECOR_INSTALL_DIR}/log4j.prod.properties
+sudo sed -i 's/log4j.appender.CONSOLE.Threshold=.*/log4j.appender.CONSOLE.Threshold=INFO/g' ${SECOR_INSTALL_DIR}/log4j.prod.properties
+sudo sed -i 's:secor.local.path=.*:secor.local.path=/tmp/secor_data/message_logs/backup:g' ${SECOR_INSTALL_DIR}/secor.prod.backup.properties
+sudo sed -i 's:log4j.appender.ROLLINGFILE.File=/mnt/:log4j.appender.ROLLINGFILE.File=/tmp/:g' ${SECOR_INSTALL_DIR}/log4j.prod.properties
+# move config files
+#sudo mkdir ${SECOR_INSTALL_DIR}/config
+#sudo mv ${SECOR_INSTALL_DIR}/*.properties ${SECOR_INSTALL_DIR}/config
--- tools/start_secor.sh
+++ tools/start_secor.sh
@@ -7,8 +7,11 @@ while [ -z "`netstat -tln | grep 9092`" ]; do
 done
 echo 'Kafka started.'
 
+# Stop child process if this main process dies
+trap "kill -- -$$" EXIT
+
 cd ${SECOR_INSTALL_DIR} && java -ea -Dsecor_group=secor_backup \
 	-Dlog4j.configuration=log4j.prod.properties \
-	-Dconfig=secor.dev.backup.properties \
+	-Dconfig=secor.prod.backup.properties \
 	-cp secor-0.16-SNAPSHOT.jar:lib/* \
 	com.pinterest.secor.main.ConsumerMain
